625fc405cf5e970284275d78b8ab4fab6b3f5257
pradeep budhathoki <pradeep.budhathoki1234@gmail.com>
2017-09-17 11:43:13 +0545

    another commit

---
+++ ExecuteScalarQuery/WebForm1.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ExecuteScalarQuery
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection("Data Source=PRADEEP\\SQLEXPRESS;Initial Catalog=product;Integrated Security=true"))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "select count(ProductId) from tblProduct";
+                cmd.Connection = con;
+                con.Open();
+                int rows=(int)cmd.ExecuteScalar();
+                Response.Write("the number of product id is " + rows);
+
+
+            }
+
+        }
+    }
+}
